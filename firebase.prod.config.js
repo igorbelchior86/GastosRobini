c8f48ed9037ea6170d522f6e40e1510ed8a467ab
igorbelchior86 <61795831+igorbelchior86@users.noreply.github.com>
2025-08-11 14:13:22 -0400

    Update firebase.prod.config.js

--- firebase.prod.config.js
+++ firebase.prod.config.js
@@ -1,11 +1,10 @@
-// firebase.config.prod.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyATGZtBlnSPnFtVgTqJ_E0xmBgzLTmMkI0",
-  authDomain: "gastosweb-e7356.firebaseapp.com",
-  databaseURL: "https://gastosweb-e7356-default-rtdb.firebaseio.com",
-  projectId: "gastosweb-e7356",
-  storageBucket: "gastosweb-e7356.firebasestorage.app",
-  messagingSenderId: "519966772782",
-  appId: "1:519966772782:web:9ec19e944e23dbe9e899bf",
-  measurementId: "G-JZYYGSJKTZ"
+  apiKey: "AIzaSyDFn1b-ngCOoiM4LvYbCnOJrlVqfrAvSzc",
+  authDomain: "gastosrobini.firebaseapp.com",
+  databaseURL: "https://gastosrobini-default-rtdb.firebaseio.com",
+  projectId: "gastosrobini",
+  storageBucket: "gastosrobini.firebasestorage.app",
+  messagingSenderId: "1080376035751",
+  appId: "1:1080376035751:web:98ffe5ceaf85ed178e4e82",
+  measurementId: "G-7E9NKHFWHJ"
 };
